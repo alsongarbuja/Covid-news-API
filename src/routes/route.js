4c8bd3ab9183efe733d04961891e3e66f94024ad
Alson33 <garbuja12alson@gmail.com>
2022-01-31 21:55:17 +0545

    add a route for base point

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
-const { getNews, getNewsFromSingleSource } = require('../controllers/news')
+const { getNews, getNewsFromSingleSource } = require('../controllers/news');
+const jsend = require('../helpers/jsend');
 
 const router = express.Router();
 
+router.get('/', (req, res) => res.json(jsend({ message: 'Thank you for using my API' })))
 router.get('/news', getNews);
 router.get('/singleSource/:newsId', getNewsFromSingleSource)
 
